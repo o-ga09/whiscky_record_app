2438d5490241e25510c440cc2c1c40db2fd97ec1
taiti09 <taitiabe1997@gmail.com>
2023-03-18 12:49:28 +0900

    fix:画面レイアウト修正

--- src/pages/Term_of_service.vue
+++ src/pages/Term_of_service.vue
@@ -1,13 +1,41 @@
 <template>
-  <div>
-    <div>
-      <h1>Routing Test Page</h1>
-    </div>
+  <div class="container">
+    <HeaderComponet title="利用規約" class="header"/>
+    <main class="body">Body</main>
+    <FooterComponet class="footer"/>
   </div>
 </template>
 
 <script>
+import FooterComponet from '../components/Footer.vue'
+import HeaderComponet from '../components/Header.vue'
+
 export default {
+  components: {FooterComponet,HeaderComponet},
   name: 'Terms_of_service'
 }
 </script>
+
+<style>
+  .container {
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+  
+  .header {
+    height: 10%;
+    background-color: #ddd;
+  }
+  
+  .body {
+    height: 80%;
+    background-color: #eee;
+  }
+  
+  .footer {
+    height: 10%;
+    background-color: #ddd;
+  }
+</style>
