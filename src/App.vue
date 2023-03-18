e69c72f9c33d1ae667bf0c48ceaeb4fbc190eed8
taiti09 <taitiabe1997@gmail.com>
2023-03-18 12:50:31 +0900

    fix:PC/Mobileに分ける処理追加

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,21 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  mounted() {
+    window.addEventListener('resize', this.handleResize)
+    this.handleResize()
+  },
+  methods: {
+    handleResize() {
+      const isDesktop = window.innerWidth >= 768 // ブレークポイントを768pxとする
+      if (isDesktop) {
+        this.$router.push({ name: 'MainPage_pc' })
+      } else {
+        this.$router.push({ name: 'MainPage_sp' })
+      }
+    },
+  },
 }
 </script>
 
