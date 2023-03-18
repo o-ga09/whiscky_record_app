dbb08d6b4f5db3128a262df541f6d5cb74ffa381
taiti09 <taitiabe1997@gmail.com>
2023-03-18 12:46:29 +0900

    feat:ヘッダー、フッター追加

---
+++ src/components/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <p>&copy; 2023 @o-ga09 All Right Reserved</p>
+</template>
+
+<script>
+export default {
+    name: 'FooterCopmonent'
+}
+</script>
+
+<style>
+p {
+    text-align: center;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>{{title}}</h1>
+</template>
+
+<script>
+export default {
+  name: 'HeaderComponet',
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+h1 {
+    text-align: center;
+}
+</style>
